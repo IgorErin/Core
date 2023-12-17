33c80d016ca4da72cf6c9d5951124bcea23c4e96
IgorErin <erin.igore@yandex.ru>
2023-12-17 15:23:22 +0300

    refactor: Praser tests

--- test/Tests/Parser.hs
+++ test/Tests/Parser.hs
@@ -13,12 +13,12 @@ run = P.calc . L.alexScanTokens
 cases :: [(String, CoreProgram)]
 cases = 
     [
-        ("f x = x; g x = let y = x in y", [("g",["x"],ELet False [("y",EVar "x")] (EVar "y")),("f",["x"],EVar "x")]),
-        ("f x = x; main = f 5",[("main",[],EAp (EVar "f") (ENum 5)),("f",["x"],EVar "x")]),
-        ("sum x y = x + y", [("sum",["y","x"],EAp (EAp (EVar "+") (EVar "x")) (EVar "y"))]),
-        ("double x = x * 2; double2 x = x + x",[("double2",["x"],EAp (EAp (EVar "+") (EVar "x")) (EVar "x")),("double",["x"],EAp (EAp (EVar "*") (EVar "x")) (ENum 2))]),
-        ("default x y = case x of <0> -> y ; <1> x -> x", [("default",["y","x"],ECase (EVar "x") [(1,["x"],EVar "x"),(0,[],EVar "y")])]),
-        ("square x = x * x ; main = square (square 3)", [("main",[],EAp (EVar "square") (EAp (EVar "square") (ENum 3))),("square",["x"],EAp (EAp (EVar "*") (EVar "x")) (EVar "x"))])
+        ("f x = x; g x = let y = x in y", [("f",["x"],EVar "x"), ("g",["x"],ELet False [("y",EVar "x")] (EVar "y"))]),
+        ("f x = x; main = f 5",[("f",["x"],EVar "x"), ("main",[],EAp (EVar "f") (ENum 5))]),
+        ("sum x y = x + y", [("sum",["x","y"],EAp (EAp (EVar "+") (EVar "x")) (EVar "y"))]),
+        ("double x = x * 2; double2 x = x + x",[("double",["x"],EAp (EAp (EVar "*") (EVar "x")) (ENum 2)), ("double2",["x"],EAp (EAp (EVar "+") (EVar "x")) (EVar "x"))]),
+        ("default x y = case x of <0> -> y ; <1> x -> x", [("default",["x","y"],ECase (EVar "x") [(0,[],EVar "y"), (1,["x"],EVar "x")])]),
+        ("square x = x * x ; main = square (square 3)", [("square",["x"],EAp (EAp (EVar "*") (EVar "x")) (EVar "x")), ("main",[],EAp (EVar "square") (EAp (EVar "square") (ENum 3)))])
     ]
 
 tests :: TestTree
