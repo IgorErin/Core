3c7b826a5cc555f24ffe8ee169636d032935864a
IgorErin <erin.igore@yandex.ru>
2023-12-17 11:48:04 +0300

    add: Heap, Node printing

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,8 +1,10 @@
 module Utils where 
 
-import qualified Data.Map as M (Map, empty, lookup, keys, size, insert, delete)
 import qualified Language as L 
+import qualified PSeq as P
+
 import qualified Data.Maybe as MB 
+import qualified Data.Map as M (Map, empty, lookup, keys, size, insert, delete, toList)
 
 ------------- Adr -------------------------
 type Addr = Int 
@@ -13,6 +15,9 @@ diffAddr = - 1
 addrIsNull :: TiHeap -> Bool 
 addrIsNull = null
 
+showAddr :: Addr -> TiHeap  -> P.T 
+showAddr addr heap = showNode (hLook addr heap) heap  
+
 type TiStack = [Addr]
 
 --------------- Node -------------------------
@@ -20,12 +25,24 @@ data Node =
     NApp Addr Addr 
     | NCombinator L.Name [L.Name] L.CoreExpr 
     | NNum Int 
+    deriving Show 
 
 isDataNode :: Node -> Bool 
 isDataNode (NNum _) = True 
 isDataNode _ = False 
+
+showNode :: Node -> TiHeap -> P.T
+showNode (NNum n) _ =  P.merge [P.str "NNum ", P.str $ show n]
+showNode (NApp f x) heap = P.merge [ P.str "NApp ", showNode nf heap , P.str " (", showNode nx heap , P.str ")"  ]
+    where 
+        nf = hLook f heap 
+        nx = hLook x heap 
+
+showNode (NCombinator name _ _ ) _ = P.merge [P.str name]
+
+
 ---------------- Dump -------------------------
-data TiDump = DummyDump
+data TiDump = DummyDump deriving Show 
 
 initDump :: TiDump
 initDump = DummyDump
@@ -53,6 +70,9 @@ hAlloc _ _ = error "empty list in hAlloc"
 
 hFree :: TiHeap -> Addr -> TiHeap 
 hFree (heap, addrs) a = (M.delete a heap, a: addrs)
+
+hShow :: TiHeap -> P.T 
+hShow heap = P.interleav P.nl . map (\(key, value) -> P.merge [P.str $ show key, P.str " -> ", showNode  value heap ]) . M.toList . fst $ heap 
 
 -------------- TiGlobals -----------------------
 type TiGlobals = M.Map L.Name Addr
